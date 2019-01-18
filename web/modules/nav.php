d5f23a73322a2c926792cf6f41031aca87f87839
silverdiva <silverdiva4u@gmail.com>
2019-01-17 20:14:02 -0800

    updated refs to .html within nav.php file

--- web/modules/nav.php
+++ web/modules/nav.php
@@ -2,6 +2,6 @@
             <label for="toggle">Show Menu</label>
             <input id="toggle" type="checkbox">
             <ul id="menu">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="assignments.html">Assignments</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="assignments.php">Assignments</a></li>
             </ul>
