695224f5ba7811443442223e51a7698c016650ea
silverdiva <silverdiva4u@gmail.com>
2019-01-20 02:47:04 -0800

    corrected wking.css to main.css in deployment folder

--- web/02-teach.php
+++ web/02-teach.php
@@ -6,7 +6,7 @@
     <meta name="description" content="CS 313 Landing Page" />
     <meta name="author" content="Kim Llanos">
     <title>CS 313 | Home </title>
-    <link href="css/wking.css" type="text/css" rel="stylesheet" />
+    <link href="css/main.css" type="text/css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 
