3e2566edac3e7935034b41f09612c0a2f970c07e
silverdiva <silverdiva4u@gmail.com>
2019-01-25 01:08:51 -0800

    updated index.html, assignments.html to .php

--- web/02-teach.php
+++ web/02-teach.php
@@ -22,8 +22,8 @@
             <label for="toggle">Show Menu</label>
             <input id="toggle" type="checkbox">
             <ul id="menu">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="assignments.html">Assignments</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="assignments.php">Assignments</a></li>
             </ul>
         </nav>
     </header>
