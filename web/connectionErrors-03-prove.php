31558f42ad42e2b8b764b5127e6b89eeb8ba29a0
silverdiva <silverdiva4u@gmail.com>
2019-02-08 16:54:18 -0800

    added Postgres connection code (05teach) to '03-prove.php'(shopping cart module

--- web/connectionErrors-03-prove.php
+++ web/connectionErrors-03-prove.php
@@ -7,33 +7,6 @@ $cartLib = new Cart();
 $products = $cartLib->pGet();
 ?>
 
-    
-<?php
-//start session to store credentials
-session_start();
-try
-{
-  $dbUrl = getenv('DATABASE');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-
-?>    
 
     <!DOCTYPE html>
     <html>
